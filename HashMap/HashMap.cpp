75278340ed79aa850bdf7c57e1f96f393eac72fa
stefanocandiani1 <scandiani22500@columbushs.com>
2024-07-28 16:13:06 -0400

    HashMap Update Commit - (07/28/2024) - [Issues on my end with the compiling I believe]

--- HashMap/HashMap.cpp
+++ HashMap/HashMap.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "HashMap.h"
+
+/// Default Constructor
+HashMap::HashMap() {
+    implementation = 1;
+    for(int i = 0; i < CAPACITY; i++) {
+        LLHashMap[i] = new LinkedList;
+    }
+    for(int i = 0; i < CAPACITY; i++) {
+        AVLHashMap[i] = new AVLTree;
+    }
+}
+
+/// Destructor
+HashMap::~HashMap() {
+    for(LinkedList* LL : LLHashMap) {
+        delete LL;
+    }
+    for(AVLTree* AVL : AVLHashMap) {
+        delete AVL;
+    }
+}
+
+/// Hash Map
+int HashMap::hashFunction(int numIngredients) {
+    return numIngredients - 4;
+}
+
+/// Insert Function
+void HashMap::insert(const string &recipeName, int recipeID, vector<string> &ingredients,
+                     int numIngredients) {
+    int hashCode = hashFunction(numIngredients);
+    LLHashMap[hashCode] -> insert(recipeName, recipeID, ingredients, numIngredients);
+    AVLHashMap[hashCode] -> Insert(recipeName, recipeID, ingredients, numIngredients);
+}
+
+/// Change implementation
+void HashMap::changeImplementation() {
+    implementation = !implementation;
+}
+
+/// Operator [] gives AVL tree object pointer at a given hash index
+template <typename T>
+T HashMap::operator[] (int numIngredients) {
+    int hashCode = hashFunction(numIngredients);
+    // Return proper implementation at a given hash index
+    if(implementation) {                // AVL Tree (Implementation = 1)
+        return AVLHashMap[hashCode];
+    } else {                            // Linked List (Implementation = 0)
+        return LLHashMap[hashCode];
+    }
+}
--- HashMap/HashMap.h
+++ HashMap/HashMap.h
@@ -6,14 +6,28 @@
 #define PROJECT3_COP3530_FRIDGE_HASHMAP_H
 
 #include <iostream>
+#include <vector>
+#include "./AVLTree/AVLTree.h"
+#include "./LinkedList/LinkedList.h"
+#define CAPACITY 14
 
 class HashMap {
 
     private:
+        LinkedList* LLHashMap[CAPACITY];
+        AVLTree* AVLHashMap[CAPACITY];
+        bool implementation;                            // 0 Linked List | 1 AVL Tree
 
+        int hashFunction(int numIngredients);
 
     public:
 
+        HashMap();
+        ~HashMap();
+        template <typename T>
+        T operator[](int numIngredients);            // Returns the Linked List head or the AVL root node
+        void insert(const std::string& recipeName, int recipeID, std::vector<string>& ingredients, int numIngredients);
+        void changeImplementation();
 
 };
 
